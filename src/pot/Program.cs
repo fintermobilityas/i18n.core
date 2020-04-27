4199f62d8f673e8c9145431aea5e06d0f3e6d5b6
Peter Rekdal Sunde <peter.sunde@gmail.com>
2020-04-27 13:28:54 +0200

    Add support for automatically rebuilding pot file based on a list of directories to scan.

--- src/pot/Program.cs
+++ src/pot/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Linq;
 using CommandLine;
 using i18n.Core;
 using i18n.Core.Abstractions.Domain;
@@ -17,16 +19,26 @@ namespace pot
         [Option("show-source-context", Required = false, HelpText = "Append source context to references")]
         public bool ShowSourceContext { get; [UsedImplicitly] set; }
 
-        [Option('w', "web-config-path", Required = false, HelpText = "Path to web.config that contain i18n.* settings.")]
+        [Option("web-config-path", Required = false, HelpText = "Path to web.config that contain i18n.* settings.")]
         public string WebConfigPath { get; [UsedImplicitly] set; }
 
         [Option("verbose", Required = false, HelpText = "Set output to verbose.")]
         public bool Verbose { get; [UsedImplicitly] set; }
+
+        [Option("watch", Required = false, HelpText = "Automatically rebuild pot if any translatable files changes.")]
+        public bool Watch { get; [UsedImplicitly] set; }
+
+        [Option("watch-delay", Required = false, HelpText = "Delay between each build (throttling). Default value is 500 ms. ")]
+        public int WatchDelay { get; [UsedImplicitly] set; } = 500;
     }
 
     [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
     internal static class Program
     {
+        static readonly object BuildLock = new object();
+        static bool _isBuilding;
+        static DateTime? _lastBuildDate;
+
         public static void Main(string[] args)
         {
             Environment.ExitCode = 1;
@@ -87,28 +99,143 @@ namespace pot
                 Console.WriteLine($"Web.config filename: {webConfigFilename}");
             }
 
-            var sw = new Stopwatch();
-            sw.Restart();
-
-            var settingsProvider = (ISettingsProvider) new SettingsProvider(projectDirectory);
-            settingsProvider.PopulateFromWebConfig(webConfigFilename);
-
-            var settings = new I18NLocalizationOptions(settingsProvider);
-            var repository = new PoTranslationRepository(settings);
-            var nuggetFinder = new FileNuggetFinder(settings);
-
-            var items = nuggetFinder.ParseAll();
-            if (repository.SaveTemplate(items))
+            if (options.Watch)
             {
-                var merger = new TranslationMerger(repository);
-                merger.MergeAllTranslation(items);
+                return Watch(options, projectDirectory, webConfigFilename, () => Build(projectDirectory, webConfigFilename, options.WatchDelay));
             }
 
-            sw.Stop();
-
-            Console.WriteLine($"Operation completed in {sw.Elapsed.TotalSeconds:F} seconds.");
+            Build(projectDirectory, webConfigFilename);
 
             return 0;
         }
+
+        static int Watch(Options options, string projectDirectory, string webConfigFilename, Action onChangeAction)
+        {
+            var settingsProvider = (ISettingsProvider)new SettingsProvider(projectDirectory);
+            settingsProvider.PopulateFromWebConfig(webConfigFilename);
+            var settings = new I18NLocalizationOptions(settingsProvider);
+            var watchers = new List<FileSystemWatcher>();
+
+            var filters = settings.WhiteList.Where(x => x.StartsWith("*.")).ToList();
+
+            Console.WriteLine($"Watching directories: {string.Join(", ", settings.DirectoriesToScan)}. Filters: {string.Join(", ", filters)}");
+
+            try
+            {
+                foreach (var directory in settings.DirectoriesToScan)
+                {
+                    if (!Directory.Exists(directory))
+                    {
+                        Console.Error.WriteLine($"Watch directory does not exist: {directory}");
+                        return 1;
+                    }
+                    
+                    var watcher = new FileSystemWatcher
+                    {
+                        Path = directory,
+                        NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName,
+                        IncludeSubdirectories = true
+                    };
+
+                    watcher.NotifyFilter = NotifyFilters.LastAccess
+                                           | NotifyFilters.LastWrite
+                                           | NotifyFilters.FileName
+                                           | NotifyFilters.DirectoryName;
+
+                    foreach (var filter in filters)
+                    {
+                        watcher.Filters.Add(filter);
+                    }
+
+                    watcher.Changed += (sender, args) =>
+                    {
+                        onChangeAction.Invoke();
+                    };
+
+                    watcher.Created += (sender, args) =>
+                    {
+                        onChangeAction.Invoke();
+                    };
+
+                    watcher.Renamed += (sender, args) =>
+                    {
+                        onChangeAction.Invoke();
+                    };
+
+                    watcher.Deleted += (sender, args) =>
+                    {
+                        onChangeAction.Invoke();
+                    };
+
+                    watcher.EnableRaisingEvents = true;
+
+                    watchers.Add(watcher);
+                }
+            }
+            finally
+            {
+                Console.ReadLine();
+
+                foreach (var watcher in watchers)
+                {
+                    watcher.Dispose();
+                }
+            }
+            
+            return 0;
+        }
+
+        static void Build(string projectDirectory, string webConfigFilename, int buildDelayMilliseconds = -1)
+        {
+            lock (BuildLock)
+            {
+                if (_isBuilding)
+                {
+                    return;
+                }
+
+                if (buildDelayMilliseconds > 0
+                    && _lastBuildDate.HasValue 
+                    && DateTime.Now - _lastBuildDate.Value < TimeSpan.FromMilliseconds(buildDelayMilliseconds))
+                {
+                    return;
+                }
+
+                _isBuilding = true;
+            }
+
+            try
+            {
+                var sw = new Stopwatch();
+                sw.Restart();
+
+                var settingsProvider = (ISettingsProvider)new SettingsProvider(projectDirectory);
+                settingsProvider.PopulateFromWebConfig(webConfigFilename);
+
+                var settings = new I18NLocalizationOptions(settingsProvider);
+                var repository = new PoTranslationRepository(settings);
+                var nuggetFinder = new FileNuggetFinder(settings);
+
+                var items = nuggetFinder.ParseAll();
+                if (repository.SaveTemplate(items))
+                {
+                    var merger = new TranslationMerger(repository);
+                    merger.MergeAllTranslation(items);
+                }
+
+                sw.Stop();
+
+                Console.WriteLine($"Build operation completed in {sw.Elapsed.TotalSeconds:F} seconds.");
+            }
+            finally
+            {
+                lock (BuildLock)
+                {
+                    _isBuilding = false;
+                    _lastBuildDate = DateTime.Now;
+                }
+            }
+        }
+
     }
 }
