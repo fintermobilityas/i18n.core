f9a1c7738bfdf588721d759d231748f0a9bb5fc6
Peter Rekdal Sunde <peter.sunde@gmail.com>
2020-06-09 20:59:28 +0200

    Add "Test-Pot" target.

--- build.ps1
+++ build.ps1
@@ -1,5 +1,8 @@
 param(
     [Parameter(Position = 0, ValueFromPipeline)]
+    [ValidateSet("Build", "Test-Pot")]
+    [string] $Target = "Build",
+    [Parameter(Position = 0, ValueFromPipeline)]
     [string] $Version = "0.0.0",
     [Parameter(Position = 1, ValueFromPipeline)]
     [ValidateSet("Debug", "Release")]
@@ -14,14 +17,51 @@ $WorkingDirectory = Split-Path -parent $MyInvocation.MyCommand.Definition
 . $WorkingDirectory\common.ps1
 
 $BuildOutputDirectory = Join-Path $WorkingDirectory build\$Version
+$NupkgsDirectory = Join-Path $WorkingDirectory nupkgs
 
 Resolve-Shell-Dependency dotnet
 
-Invoke-Command-Colored dotnet @(
-    ("build {0}" -f (Join-Path $WorkingDirectory i18n.core.sln))
-    "/p:Version=$Version",
-    "/p:GeneratePackageOnBuild=$Nupkg"
-    "/p:IsCIBuild=$CI"
-    "--output $BuildOutputDirectory"
-    "--configuration $Configuration"
-)
+switch($Target) {
+    "Build" {
+        Invoke-Command-Colored dotnet @(
+            ("build {0}" -f (Join-Path $WorkingDirectory i18n.core.sln))
+            "/p:Version=$Version",
+            "/p:GeneratePackageOnBuild=$Nupkg"
+            "/p:IsCIBuild=$CI"
+            "--output $BuildOutputDirectory"
+            "--configuration $Configuration"
+        )
+    }
+    "Test-Pot" {
+
+        $PotFileName = Join-Path $WorkingDirectory src\i18n.Demo\locale\messages.pot
+        $WebConfigFilename = Join-Path $WorkingDirectory src\i18n.Demo\Web.config
+
+        Invoke-Command-Colored dotnet @(
+            "tool update pot"
+            "--global"
+            "--version $Version"
+            "--add-source ""$NupkgsDirectory"""
+        )
+
+        Invoke-Command-Colored pot @("--web-config-path $WebConfigFilename")
+
+        $LocaleExists = Test-Path $PotFileName -PathType Leaf
+        if($LocaleExists -eq $false) {
+            exit 1
+        }
+
+        $PotCreationDate = Get-Content $PotFileName | Select-Object -First 4 `
+            | Where-Object { [string]::IsNullOrWhiteSpace($_) -eq $false } `
+            | Select-Object -Last 1
+
+        $IsValidPotStr = ($PotCreationDate -replace "`"", "").StartsWith("POT-Creation-Date");
+        if($IsValidPotStr -eq $false) {
+            exit 1
+        }
+
+        Write-Output-Colored "Success"
+        exit 0
+          
+    }
+}
