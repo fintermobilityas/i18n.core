615e484de536b50feb45242f0c506d572fae6d2f
Peter Rekdal Sunde <peter.sunde@gmail.com>
2020-09-17 14:36:28 +0200

    Add demo pdf in order to test that middleware does not attempt to translate unsupported content types.

--- src/i18n.Demo/Controllers/HomeController.cs
+++ src/i18n.Demo/Controllers/HomeController.cs
@@ -1,21 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using i18n.Demo.Models;
+using Microsoft.AspNetCore.Hosting;
 
 namespace i18n.Demo.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -26,6 +30,12 @@ namespace i18n.Demo.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<IActionResult> DownloadPdf()
+        {
+            var pdfBytes = await System.IO.File.ReadAllBytesAsync(Path.Combine(_webHostEnvironment.WebRootPath, "assets", "sample.pdf"));
+            return File(pdfBytes, "application/pdf", "sample.pdf");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
